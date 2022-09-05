ce3abbbb8c86b3918676295312694160399bec3e
miralmag <miriam.almagro@gmail.com>
2022-09-05 13:16:16 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+const port = 4000;
+server.listen (port, () => {
+    console.log("listening" + port)
+});
+
+//endpoints
+
+server.post("/card", (req, resp) => {
+    const responseSuccess = {
+        cardURL: "https://awesome-profile.herokuapp.com/card/",
+        success: true
+    }
+
+    const responseError = {
+        success: false,
+        error: "Faltan datos"
+    }
+
+    resp.json(responseError);
+})
