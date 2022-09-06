a4eb3b29912fb2fd0e8a5989ee0e82fb2c9d0366
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-09-06 13:08:27 +0200

    create static server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
@@ -7,22 +7,25 @@ server.use(cors());
 server.use(express.json());
 
 const port = 4000;
-server.listen (port, () => {
-    console.log("listening" + port)
+server.listen(port, () => {
+  console.log('listening' + port);
 });
 
 //endpoints
 
-server.post("/card", (req, resp) => {
-    const responseSuccess = {
-        cardURL: "https://awesome-profile.herokuapp.com/card/",
-        success: true
-    }
+server.post('/card', (req, resp) => {
+  const responseSuccess = {
+    cardURL: 'https://awesome-profile.herokuapp.com/card/',
+    success: true,
+  };
 
-    const responseError = {
-        success: false,
-        error: "Faltan datos"
-    }
+  const responseError = {
+    success: false,
+    error: 'Faltan datos',
+  };
 
-    resp.json(responseError);
-})
+  resp.json(responseError);
+});
+
+const staticServer = './web';
+server.use(express.static(staticServer));
