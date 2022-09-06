91656b567e5eba831304eaa1664f78b116d14cc7
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-09-06 13:25:55 +0200

    create server post card

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const cors = require('cors');
 
 const server = express();
 
 server.use(cors());
-server.use(express.json());
+server.use(express.json({ limit: '10mb' }));
 
 const port = 4000;
 server.listen(port, () => {
   console.log('listening' + port);
 });
 
+const saveCard = [];
+
 //endpoints
 
 server.post('/card', (req, resp) => {
-  const responseSuccess = {
-    cardURL: 'https://awesome-profile.herokuapp.com/card/',
-    success: true,
-  };
-
-  const responseError = {
-    success: false,
-    error: 'Faltan datos',
-  };
-
-  resp.json(responseError);
+  if (
+    req.body.palette === '' &&
+    req.body.name === '' &&
+    req.body.job === '' &&
+    req.body.phone === '' &&
+    req.body.email === '' &&
+    req.body.linkedin === '' &&
+    req.body.github === '' &&
+    req.body.photo === ''
+  ) {
+    const responseError = {
+      success: false,
+      error: 'Faltan datos',
+    };
+  } else {
+    const newCard = {
+      id: uuidv4(),
+      ...req.body,
+    };
+    saveCard.push(newCard);
+    const responseSuccess = {
+      cardURL: `http://localhost:4000/card/${newCard.id}`,
+      success: true,
+    };
+    console.log(req.body);
+    resp.json(responseSuccess);
+  }
 });
 
 const staticServer = './web';
