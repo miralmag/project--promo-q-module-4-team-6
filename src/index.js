e918f3be8ff552dc9b4dcfa5930d78319eaa634a
miralmag <miriam.almagro@gmail.com>
2022-09-21 10:50:52 +0200

    fix url name

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ server.post('/card', (req, resp) => {
     console.log(result);
     if (result !== undefined) {
       const responseSuccess = {
-        cardURL: `https://batwomen.herokuapp.com/card/${newCard.id}`,
+        cardURL: `https://batwomenmiralmag.herokuapp.com/card/${newCard.id}`,
         success: true,
       };
       resp.json(responseSuccess);
